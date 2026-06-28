38b745b1ffa306679b48a05354052a5696e542fe
Jules Martins <fearlesslymediagroup@gmail.com>
2026-06-28 15:42:38 -0700

    chore: remove temporary scripting files and shellcheck binaries, update .gitignore

--- fix_ip_sh.py
+++
@@ -1,7 +0,0 @@
-with open('plugins/ip.sh', 'r') as f:
-    content = f.read()
-
-content = content.replace("tr -d 'addr:'", "sed 's/addr://'")
-
-with open('plugins/ip.sh', 'w') as f:
-    f.write(content)
--- fix_test.py
+++
@@ -1,7 +0,0 @@
-with open('tests/test.sh', 'r') as f:
-    content = f.read()
-
-content = content.replace('line_count=$(echo "$no_ascii_out" | grep -v "^$" | wc -l)', 'line_count=$(echo "$no_ascii_out" | grep -c -v "^$" || true)')
-
-with open('tests/test.sh', 'w') as f:
-    f.write(content)
